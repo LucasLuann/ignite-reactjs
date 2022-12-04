439482c8aa0f7b09d82257e63b65e3b5ede9598e
Lucas Luann <lucasluann10@gmail.com>
2022-12-03 20:47:36 -0400

    componente header finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import styles from './Header.module.css';
 
+import igniteLogo from '../images/ignite-logo.svg';
+
+console.log(igniteLogo);
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={igniteLogo} alt="Logotipo do Projeto" />
     </header>
   );
 }
