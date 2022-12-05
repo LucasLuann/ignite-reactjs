ecd04227d632c0e6d2d404dfee38bf00b08b70b6
Lucas Luann <lucasluann10@gmail.com>
2022-12-04 23:59:12 -0400

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,28 @@ import './global.css';
 
 import styles from './App.module.css';
 
-function App() {
+import { Post } from './Post';
+
+import { SideBar } from './components/Sidebar';
+
+export function App() {
   return (
     <div>
       <Header />
 
       <div className={styles.wrapper}>
-        <aside>SideBar</aside>
-        <main></main>
+        <SideBar />
+        <main>
+          <Post
+            author="Lucas Luann"
+            content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolor officiis alias ab labore facilis animi nostrum culpa, modi, eum est illum repellendus esse reprehenderit. Labore ipsam ad magnam amet."
+          />
+          <Post
+            author="Giovanna Ágatha"
+            content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolor officiis alias ab labore facilis animi nostrum culpa, modi, eum est illum repellendus esse reprehenderit. Labore ipsam ad magnam amet."
+          />
+        </main>
       </div>
     </div>
-  )
+  );
 }
-
-export default App;
