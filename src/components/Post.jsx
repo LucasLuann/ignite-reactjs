7a684e46efb741241e2ef8eff240628b0585c6de
Lucas Brito <lucasluann10@gmail.com>
2022-12-10 14:34:44 -0400

    componente post pt2

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,7 +9,17 @@ export function Post(){
                         <span>Front-end Developer</span>
                     </div>
                 </div>
+                <time title="10 de dezembro às 14:22h" dateTime="2022-12-10 14:26:30">Publicadp há 1h</time> 
             </header>
-        </article>
+            <div className={styles.content}>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit veniam quae molestiae esse aperiam inventore error, id at blanditiis? Placeat exercitationem necessitatibus laborum! Cupiditate error animi eos quidem esse quisquam!👋</p>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit veniam quae molestiae esse aperiam inventore error, id at blanditiis? Placeat exercitationem necessitatibus laborum! Cupiditate error animi eos quidem esse quisquam!👋</p>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit veniam quae molestiae esse aperiam inventore error, id at blanditiis? Placeat exercitationem necessitatibus laborum! Cupiditate error animi eos quidem esse quisquam!👋</p>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit veniam quae molestiae esse aperiam inventore error, id at blanditiis? Placeat exercitationem necessitatibus laborum! Cupiditate error animi eos quidem esse quisquam!👋</p>
+            </div>
+        </article> 
     )
 }
