6d55ea57dbedfbc33e37cc7f8fce4400f5e4888a
Lucas Luann <lucasluann10@gmail.com>
2023-01-20 20:31:33 -0400

    iterando o componente de post.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,43 @@ import './global.css';
 
 import styles from './App.module.css';
 
+const posts = [
+  {
+    id: 1,
+    autor: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/75109074?v=4',
+      nome: 'Lucas Luann',
+      cargo: 'Desenvolvedor Front-End Jr.',
+    },
+    conteudo: [
+      { tipo: 'paragrafo', conteudo: 'Fala galeraa' },
+      {
+        tipo: 'paragrafo',
+        conteudo: 'Subindo mais um projetinho no github...',
+      },
+      { tipo: 'link', conteudo: 'teste.teste/lucas' },
+    ],
+    dataPublicacao: new Date('2023-01-20 20:18:00'),
+  },
+  {
+    id: 2,
+    autor: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/75109074?v=4',
+      nome: 'Lucas Luann',
+      cargo: 'Desenvolvedor Front-End Jr.',
+    },
+    conteudo: [
+      { tipo: 'paragrafo', conteudo: 'Fala galeraa' },
+      {
+        tipo: 'paragrafo',
+        conteudo: 'Subindo mais um projetinho no github...',
+      },
+      { tipo: 'link', conteudo: 'teste.teste/lucas' },
+    ],
+    dataPublicacao: new Date('2023-01-19 10:18:00'),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -13,10 +50,15 @@ export function App() {
       <div className={styles.wrapper}>
         <SideBar />
         <main>
-          <Post />
-          <Post />
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                autor={post.autor}
+                conteudo={post.conteudo}
+                dataPublicacao={post.dataPublicacao}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
